1e8e4f5dadf16d6b52c6ff6d53c198290a659833
ColdCoffee4127 <2070131254@qq.com>
2024-09-24 13:44:53 +0800

    change

---
+++ springdemoaop/src/main/java/com/coffee/Main.java
@@ -0,0 +1,13 @@
+package com.coffee;
+
+import com.coffee.dao.StuDao;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        StuDao stuDao = (StuDao) context.getBean("stuDao");
+        stuDao.insert();
+    }
+}
